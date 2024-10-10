f46605e4dd6c70d852d8691d8aca4ae2038fdb7d
Guilherme.MD <gui.martins.duarte@gmail.com>
2024-10-10 19:35:27 -0300

    Pygame, tamanho tela, abrir e fechar tela, update .gitignore
    Signed-off-by: Guilherme.MD <gui.martins.duarte@gmail.com>

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import pygame
 
+print('Setup Start')  # Comment that only appears in the terminal to visualize events
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))  # Game screen scaling
+print('Setup End')
 
+print('Loop Start')
+while True:
+    # confere todos os eventos / check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Variável QUIT = 256
+            pygame.quit()  # Close Window
+            quit()  # End pygame
+
+# Criar um evento para FECHAR a janela.
