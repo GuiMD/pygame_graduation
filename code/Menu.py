b47de8f65a7b60515edc8920743b7a2b32850f74
Guilherme.MD <gui.martins.duarte@gmail.com>
2024-10-13 23:52:36 -0300

    Menu Screen + Background Image
    Signed-off-by: Guilherme.MD <gui.martins.duarte@gmail.com>

--- code/Menu.py
+++ code/Menu.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 #*** coding: utf-8 ***
+import pygame.image
 
 class Menu:
-    def __init__(self, windows):
+    def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)  # Create a rectangle on the home screen to place the image.
+
     def run(self, ):
+        self.window.blit(source=self.surf, dest=self.rect)  # Place the image in the rectangle.
+        pygame.display.flip()  # Update the images on the entire screen.
         pass
